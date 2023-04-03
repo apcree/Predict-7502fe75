7190153847e1b89532450490a73b2c571ae5d63a
barrettjason <barrett.jason.jb@gmail.com>
2023-04-03 22:16:53 +1000

    import library success

---
+++ predict.py
@@ -0,0 +1,14 @@
+# https://www.kaggle.com/code/gusthema/house-prices-prediction-using-tfdf
+
+# import the library
+#----------------------------------------------------------
+
+
+import tensorflow as tf
+import tensorflow_decision_forests as tfdf
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Comment this if the data visualisations doesn't work on your side
+# %matplotlib inline
